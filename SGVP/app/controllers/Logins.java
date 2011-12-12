2894508c313f475a9202dc0bcb5f74a6a620febe
Dionei Muller <dioneidj@yahoo.com.br>
2011-12-12 21:12:57 -0200

    teste trabalho

---
+++ SGVP/app/controllers/Logins.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controllers;
+
+/**
+ *
+ * @author Elisson
+ */
+public class Logins extends Application{
+
+    public static void index(){
+
+        render();
+
+
+
+    }
+
+}
